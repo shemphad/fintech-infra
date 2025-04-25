39951bc56107a01438cbbe076da441055e76c4a9
Ndiforamang Fusi <fusisoft@gmail.com>
2025-04-24 21:58:51 -0500

    fixed attribute errors

--- modules/eks-cluster/providers.tf
+++ modules/eks-cluster/providers.tf
@@ -3,9 +3,9 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster" "this" {
+#   name = var.cluster_name
+# }
 
 # data "aws_eks_cluster_auth" "this" {
 #   name = var.cluster_name
