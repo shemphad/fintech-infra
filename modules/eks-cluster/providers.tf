dbf9dd97ffc6d6a6ad2bd817f18c80d319f466b0
Ndiforamang Fusi <fusisoft@gmail.com>
2025-04-24 22:05:06 -0500

    fixed attribute errors

--- modules/eks-cluster/providers.tf
+++ modules/eks-cluster/providers.tf
@@ -11,9 +11,9 @@ provider "aws" {
 #   name = var.cluster_name
 # }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.this.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.this.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#   # token                  = data.aws_eks_cluster_auth.this.token
+# }
 
