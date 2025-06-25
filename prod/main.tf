97455e20290774ebe5b6233ff14c3f5db063356d
Ndiforamang Fusi <fusisoft@gmail.com>
2025-06-25 17:31:19 -0500

    added enhancement on eks module

--- prod/main.tf
+++ prod/main.tf
@@ -12,10 +12,10 @@ module "vpc" {
 # ################################################################################
 
 module "eks" {
-  source            = "./../modules/eks-cluster"
-  cluster_name      = var.cluster_name
-  rolearn           = var.rolearn
-  cni_role_arn      = module.iam.cni_role_arn
+  source             = "./../modules/eks-cluster"
+  cluster_name       = var.cluster_name
+  rolearn            = var.rolearn
+  cni_role_arn       = module.iam.cni_role_arn
   security_group_ids = [module.eks-client-node.eks_client_sg]
   vpc_id             = module.vpc.vpc_id
   private_subnets    = module.vpc.private_subnets
@@ -137,9 +137,9 @@ module "iam" {
   environment       = var.env_name
   aws_region        = var.aws_region
   aws_account_id    = var.aws_account_id
-  eks_oidc_provider  = local.eks_oidc_provider
+  eks_oidc_provider = local.eks_oidc_provider
   tags              = local.common_tags
-  depends_on = [module.eks]
+  depends_on        = [module.eks]
 
 
 }
