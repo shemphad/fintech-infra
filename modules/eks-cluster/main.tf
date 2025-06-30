90be9553ee89f4b8430ad4576edd087bdc4e5c26
Ndiforamang Fusi <fusisoft@gmail.com>
2025-06-30 05:18:43 -0500

    added worker nodes

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -20,55 +20,38 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
   cluster_endpoint_public_access           = true
-  bootstrap_self_managed_addons           = true
+  bootstrap_self_managed_addons            = true
 
   vpc_id                                = var.vpc_id
   subnet_ids                            = var.private_subnets
   control_plane_subnet_ids              = var.private_subnets
   cluster_additional_security_group_ids = var.security_group_ids
 
-  create_cloudwatch_log_group = true
-  cluster_enabled_log_types   = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
-
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
+    coredns = { most_recent = true }
+    kube-proxy = { most_recent = true }
     vpc-cni = {
       most_recent               = true
       service_account_role_arn = var.cni_role_arn
     }
-    eks-pod-identity-agent = {
-      most_recent = true
-    }
-  }
-
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2023_x86_64_STANDARD"
-    instance_types = ["t2.medium"]
-    iam_role_additional_policies = {
-      AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-    }
-  }
-
-  eks_managed_node_groups = {
-    eks-node-group-2 = {
-      min_size     = 2
-      desired_size = 2
-      max_size     = 10
-    }
+    eks-pod-identity-agent = { most_recent = true }
   }
 
   access_entries = {
+    terraform_user = {
+      kubernetes_groups = ["system:masters"]
+      principal_arn     = "arn:aws:iam::999568710647:user/fusi"
+
+      policy_associations = [
+        {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      ]
+    }
+
     github_runner = {
       kubernetes_groups = ["eks-admins"]
       principal_arn     = "arn:aws:iam::999568710647:role/github-runner-ssm-role"
@@ -88,8 +71,6 @@ module "eks" {
 }
 
 resource "kubernetes_cluster_role_binding" "eks_admins_binding" {
-  provider = kubernetes.eks
-
   metadata {
     name = "eks-admins-binding"
   }
