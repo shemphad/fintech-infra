48468fbb67625c81a87ccb7563ce9c321d41f2d8
Ndiforamang Fusi <fusisoft@gmail.com>
2025-07-03 05:32:33 -0500

    Added eks user to cluster configmap

--- modules/eks-cluster/providers.tf
+++ modules/eks-cluster/providers.tf
@@ -4,6 +4,18 @@ provider "aws" {
 }
 
 
+provider "kubernetes" {
+  alias = "eks"
+  host  = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+  }
+}
+
+
 # provider "kubernetes" {
 #   host                   = module.eks.cluster_endpoint
 #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
