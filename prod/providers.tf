8ed506d6f8f644ba39821cafd9b6b2616a324a84
Ndiforamang Fusi <fusisoft@gmail.com>
2025-07-03 05:19:53 -0500

    Added eks user to cluster configmap

--- prod/providers.tf
+++ prod/providers.tf
@@ -1,11 +1,14 @@
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
+
+  depends_on = [module.eks]
 }
 
 provider "helm" {
@@ -15,9 +18,11 @@ provider "helm" {
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
+
+  depends_on = [module.eks]
 }
 
 # provider "kubernetes" {
