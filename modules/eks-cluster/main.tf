4c009ecb98e615c5fc34e984c81637f335ae1118
Ndiforamang Fusi <fusisoft@gmail.com>
2025-07-03 08:44:48 -0500

    Added eks user to cluster configmap

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -1,9 +1,5 @@
-##############################################
-# Data + Auth
-##############################################
-
 data "aws_eks_cluster_auth" "main" {
-  name = module.eks.cluster_name
+  name = var.cluster_name
 }
 
 data "aws_caller_identity" "current" {}
@@ -11,16 +7,9 @@ data "aws_caller_identity" "current" {}
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+  token                  = data.aws_eks_cluster_auth.main.token
+  alias                  = "eks"
 }
-
-##############################################
-# EKS Control Plane
-##############################################
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -29,64 +18,44 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.31"
 
-  enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = true # Keep for legacy bootstrap
   cluster_endpoint_public_access           = true
+  bootstrap_self_managed_addons            = true
 
-  bootstrap_self_managed_addons = false
-
-  vpc_id                   = var.vpc_id
-  subnet_ids               = var.private_subnets
-  control_plane_subnet_ids = var.private_subnets
+  vpc_id                                = var.vpc_id
+  subnet_ids                            = var.private_subnets
+  control_plane_subnet_ids              = var.private_subnets
   cluster_additional_security_group_ids = var.security_group_ids
 
-  create_cloudwatch_log_group = true
-  cluster_enabled_log_types   = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-
   cluster_addons = {
-    coredns = {
-      most_recent       = true
-      resolve_conflicts = "OVERWRITE"
-    }
-    kube-proxy = {
-      most_recent       = true
-      resolve_conflicts = "OVERWRITE"
-    }
-    vpc-cni = {
-      most_recent              = true
-      service_account_role_arn = var.cni_role_arn
-      resolve_conflicts        = "OVERWRITE"
-    }
-    eks-pod-identity-agent = {
-      most_recent       = true
-      resolve_conflicts = "OVERWRITE"
-    }
+  coredns = {
+    most_recent       = true
+    resolve_conflicts = "OVERWRITE"
   }
 
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2023_x86_64_STANDARD"
-    instance_types = ["t3.medium"]
-
-    min_size     = 1
-    max_size     = 5
-    desired_size = 2
-
-    iam_role_additional_policies = {
-      AmazonEKSWorkerNodePolicy          = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-      AmazonEKS_CNI_Policy               = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-      AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-    }
+  kube-proxy = {
+    most_recent       = true
+    resolve_conflicts = "OVERWRITE"
   }
 
-  eks_managed_node_groups = {
-    eks-node-group-1 = {
-      # Uses defaults above
-    }
+  vpc-cni = {
+    most_recent               = true
+    service_account_role_arn = var.cni_role_arn
+    resolve_conflicts         = "OVERWRITE"
   }
+
+  eks-pod-identity-agent = {
+    most_recent       = true
+    resolve_conflicts = "OVERWRITE"
+  }
+}
+
 
   access_entries = {
-    fusi = {
-      kubernetes_groups = ["eks-admins"]
-      principal_arn     = "arn:aws:iam::999568710647:user/fusi"
+    terraform_user = {
+      # ✅ Use a custom group, NOT system:masters
+      kubernetes_groups = ["platform-admins"]
+      principal_arn     = "arn:aws:iam::999568710647:user/nfusi"
 
       policy_associations = [
         {
@@ -116,10 +85,7 @@ module "eks" {
   tags = local.common_tags
 }
 
-##############################################
-# RBAC Bindings with depends_on
-##############################################
-
+# Bind terraform_user to cluster-admin
 resource "kubernetes_cluster_role_binding" "platform_admins_binding" {
   metadata {
     name = "platform-admins-binding"
@@ -136,10 +102,9 @@ resource "kubernetes_cluster_role_binding" "platform_admins_binding" {
     name      = "platform-admins"
     api_group = "rbac.authorization.k8s.io"
   }
-
-  depends_on = [module.eks]
 }
 
+# Bind github_runner to cluster-admin
 resource "kubernetes_cluster_role_binding" "eks_admins_binding" {
   metadata {
     name = "eks-admins-binding"
@@ -156,13 +121,12 @@ resource "kubernetes_cluster_role_binding" "eks_admins_binding" {
     name      = "eks-admins"
     api_group = "rbac.authorization.k8s.io"
   }
-
-  depends_on = [module.eks]
 }
 
-##############################################
-# Kubernetes Namespaces with depends_on
-##############################################
+
+################################################################################
+# Kubernetes Namespaces
+################################################################################
 
 resource "kubernetes_namespace" "fintech" {
   metadata {
@@ -174,8 +138,6 @@ resource "kubernetes_namespace" "fintech" {
       app = "fintech"
     }
   }
-
-  depends_on = [module.eks]
 }
 
 resource "kubernetes_namespace" "monitoring" {
@@ -188,8 +150,6 @@ resource "kubernetes_namespace" "monitoring" {
       app = "monitoring"
     }
   }
-
-  depends_on = [module.eks]
 }
 
 resource "kubernetes_namespace" "fintech_dev" {
@@ -202,6 +162,211 @@ resource "kubernetes_namespace" "fintech_dev" {
       app = "fintech-dev"
     }
   }
-
-  depends_on = [module.eks]
 }
+# ##############################################
+# # Data + Auth
+# ##############################################
+
+# data "aws_eks_cluster_auth" "main" {
+#   name = module.eks.cluster_name
+# }
+
+# data "aws_caller_identity" "current" {}
+
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "aws"
+#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+#   }
+# }
+
+# ##############################################
+# # EKS Control Plane
+# ##############################################
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "~> 20.0"
+
+#   cluster_name    = var.cluster_name
+#   cluster_version = "1.31"
+
+#   enable_cluster_creator_admin_permissions = true
+#   cluster_endpoint_public_access           = true
+
+#   bootstrap_self_managed_addons = false
+
+#   vpc_id                   = var.vpc_id
+#   subnet_ids               = var.private_subnets
+#   control_plane_subnet_ids = var.private_subnets
+#   cluster_additional_security_group_ids = var.security_group_ids
+
+#   create_cloudwatch_log_group = true
+#   cluster_enabled_log_types   = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+#   cluster_addons = {
+#     coredns = {
+#       most_recent       = true
+#       resolve_conflicts = "OVERWRITE"
+#     }
+#     kube-proxy = {
+#       most_recent       = true
+#       resolve_conflicts = "OVERWRITE"
+#     }
+#     vpc-cni = {
+#       most_recent              = true
+#       service_account_role_arn = var.cni_role_arn
+#       resolve_conflicts        = "OVERWRITE"
+#     }
+#     eks-pod-identity-agent = {
+#       most_recent       = true
+#       resolve_conflicts = "OVERWRITE"
+#     }
+#   }
+
+#   eks_managed_node_group_defaults = {
+#     ami_type       = "AL2023_x86_64_STANDARD"
+#     instance_types = ["t3.medium"]
+
+#     min_size     = 1
+#     max_size     = 5
+#     desired_size = 2
+
+#     iam_role_additional_policies = {
+#       AmazonEKSWorkerNodePolicy          = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#       AmazonEKS_CNI_Policy               = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#       AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#     }
+#   }
+
+#   eks_managed_node_groups = {
+#     eks-node-group-1 = {
+#       # Uses defaults above
+#     }
+#   }
+
+#   access_entries = {
+#     fusi = {
+#       kubernetes_groups = ["eks-admins"]
+#       principal_arn     = "arn:aws:iam::999568710647:user/fusi"
+
+#       policy_associations = [
+#         {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#           access_scope = {
+#             type = "cluster"
+#           }
+#         }
+#       ]
+#     }
+
+#     github_runner = {
+#       kubernetes_groups = ["eks-admins"]
+#       principal_arn     = "arn:aws:iam::999568710647:role/github-runner-ssm-role"
+
+#       policy_associations = [
+#         {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#           access_scope = {
+#             type = "cluster"
+#           }
+#         }
+#       ]
+#     }
+#   }
+
+#   tags = local.common_tags
+# }
+
+# ##############################################
+# # RBAC Bindings with depends_on
+# ##############################################
+
+# resource "kubernetes_cluster_role_binding" "platform_admins_binding" {
+#   metadata {
+#     name = "platform-admins-binding"
+#   }
+
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+
+#   subject {
+#     kind      = "Group"
+#     name      = "platform-admins"
+#     api_group = "rbac.authorization.k8s.io"
+#   }
+
+#   depends_on = [module.eks]
+# }
+
+# resource "kubernetes_cluster_role_binding" "eks_admins_binding" {
+#   metadata {
+#     name = "eks-admins-binding"
+#   }
+
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+
+#   subject {
+#     kind      = "Group"
+#     name      = "eks-admins"
+#     api_group = "rbac.authorization.k8s.io"
+#   }
+
+#   depends_on = [module.eks]
+# }
+
+# ##############################################
+# # Kubernetes Namespaces with depends_on
+# ##############################################
+
+# resource "kubernetes_namespace" "fintech" {
+#   metadata {
+#     name = "fintech"
+#     annotations = {
+#       name = "fintech"
+#     }
+#     labels = {
+#       app = "fintech"
+#     }
+#   }
+
+#   depends_on = [module.eks]
+# }
+
+# resource "kubernetes_namespace" "monitoring" {
+#   metadata {
+#     name = "monitoring"
+#     annotations = {
+#       name = "monitoring"
+#     }
+#     labels = {
+#       app = "monitoring"
+#     }
+#   }
+
+#   depends_on = [module.eks]
+# }
+
+# resource "kubernetes_namespace" "fintech_dev" {
+#   metadata {
+#     name = "fintech-dev"
+#     annotations = {
+#       name = "fintech-dev"
+#     }
+#     labels = {
+#       app = "fintech-dev"
+#     }
+#   }
+
+#   depends_on = [module.eks]
+# }
